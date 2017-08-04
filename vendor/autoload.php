5de26ff05fdd43a25ef7eb3d2eec70236be38e49
Jenkins <jenkins@computerminds.co.uk>
2017-08-04 13:52:39 +0000

    Built Deployable files from Drush make * bdeacfb Merge branch 'staging' into production * 17e79b8 Issue #43706: Explain colour shading a bit more for areas.. * f4918bb Issue #43621: Timeline browser is tricky to use on iPad. Issue #43624: Two-finger zoom on the comparative timeline zooms the whole page. * 9002c3d exclude apache server-status as required by our hosts * 3f8596b reduce the size of the Warwick Logo to match the height of the timeline (60px) * c0ea41f swapout copyright for Warwick Logo * bc1d9b5 Issue #44161: Pin popup UI on mobile - a little unintuitive. * ea3a2fd Issue #44159: Timeline range selector handle inconsistent in look. * 9b6c5b7 Issue #44160: Sidebar UI in mobile has no scrolling. * 8c2c3a0 Issue #44162: Cursor behaviour on hover over search results. * cd82d4e Issue #44158: Additional tick-box on registration form.

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit58049b1764cf0ac969b6d97684909ec2::getLoader();
+return ComposerAutoloaderInit925d40d865e35bc3d8ae8a4a2929a278::getLoader();
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit58049b1764cf0ac969b6d97684909ec2
+class ComposerStaticInit925d40d865e35bc3d8ae8a4a2929a278
 {
     public static $files = array (
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
@@ -5084,11 +5084,11 @@ class ComposerStaticInit58049b1764cf0ac969b6d97684909ec2
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit58049b1764cf0ac969b6d97684909ec2::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit58049b1764cf0ac969b6d97684909ec2::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit58049b1764cf0ac969b6d97684909ec2::$prefixesPsr0;
-            $loader->fallbackDirsPsr0 = ComposerStaticInit58049b1764cf0ac969b6d97684909ec2::$fallbackDirsPsr0;
-            $loader->classMap = ComposerStaticInit58049b1764cf0ac969b6d97684909ec2::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit925d40d865e35bc3d8ae8a4a2929a278::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit925d40d865e35bc3d8ae8a4a2929a278::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit925d40d865e35bc3d8ae8a4a2929a278::$prefixesPsr0;
+            $loader->fallbackDirsPsr0 = ComposerStaticInit925d40d865e35bc3d8ae8a4a2929a278::$fallbackDirsPsr0;
+            $loader->classMap = ComposerStaticInit925d40d865e35bc3d8ae8a4a2929a278::$classMap;
 
         }, null, ClassLoader::class);
     }
