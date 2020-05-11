d5bb6c2f7bb8ac4895b8d15c3e74edc182f2ef37
Steven Jones <steven.jones@computerminds.co.uk>
2020-05-11 09:08:15 +0100

    Add an on-deploy script.

---
+++ scripts/shell/on-deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -xe
+
+set -x
+set -e
+
+alias="$1.$2"
+
+drush "@$alias" updb -y --entity-updates --cache-clear=0
+
+# The outer drush thread from the previous command would incorrectly overwrite
+# the newly-rebuilt cache of hook implementations that its inner thread(s)
+# would have written. Simplest to just clear caches with a separate command.
+# This will include clearing the drush cache, which was previously done here on
+# its own.
+drush "@$alias" -y cache-rebuild
+
+# Revert config.
+#drush "@$alias" cmci -y
+
+drush "@$alias" updb -y --entity-updates
+
+drush "@$alias" cache-rebuild -y
