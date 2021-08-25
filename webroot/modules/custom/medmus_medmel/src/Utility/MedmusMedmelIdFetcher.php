4dd959c52b3c1eb9f3ef7973374db4a8f07fcc0f
Steven Jones <steven.jones@computerminds.co.uk>
2021-08-25 09:31:35 +0100

    Handle guzzle exceptions.

--- webroot/modules/custom/medmus_medmel/src/Utility/MedmusMedmelIdFetcher.php
+++ webroot/modules/custom/medmus_medmel/src/Utility/MedmusMedmelIdFetcher.php
@@ -6,6 +6,7 @@ use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Database\Connection;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\RequestException;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -64,38 +65,51 @@ class MedmusMedmelIdFetcher implements ContainerInjectionInterface {
   function syncIds() {
     $config = $this->configFactory->get('medmus_medmel.settings');
 
-    // Fetch the IDs, and make our DB table look like the JSON.
-    $request = $this->httpClient->request('GET', $config->get('fetchUrl'));
+    try {
+      // Fetch the IDs, and make our DB table look like the JSON.
+      $request = $this->httpClient->request('GET', $config->get('fetchUrl'));
 
-    if ($request->getStatusCode() != 200) {
-      $this->logger->error('Got error code: @code', [
-        '@code' => $request->getStatusCode(),
-      ]);
-    }
-    else {
-      // Process the data.
-      $contents = json_decode($request->getBody()->getContents(), TRUE);
-      if (!empty($contents) && is_array($contents)) {
-        // This will ensure that our delete all and then re-insert everything works atomically.
-        $transaction = $this->database->startTransaction();
-        $this->database
-          ->delete('medmus_medmel')
-          ->execute();
-        $insert = $this->database
-          ->insert('medmus_medmel')
-          ->fields(['unique_id', 'work_id', 'ms']);
-        foreach ($contents as $content) {
-          $insert->values(['unique_id' => $content['unique_id'], 'work_id' => $content['work_id'], 'ms' => $content['ms']]);
+      if ($request->getStatusCode() != 200) {
+        $this->logger->error('Got error code: @code', [
+          '@code' => $request->getStatusCode(),
+        ]);
+      }
+      else {
+        // Process the data.
+        $contents = json_decode($request->getBody()->getContents(), TRUE);
+        if (!empty($contents) && is_array($contents)) {
+          // This will ensure that our delete all and then re-insert everything works atomically.
+          $transaction = $this->database->startTransaction();
+          $this->database
+            ->delete('medmus_medmel')
+            ->execute();
+          $insert = $this->database
+            ->insert('medmus_medmel')
+            ->fields(['unique_id', 'work_id', 'ms']);
+          foreach ($contents as $content) {
+            $insert->values(['unique_id' => $content['unique_id'], 'work_id' => $content['work_id'], 'ms' => $content['ms']]);
+          }
+          $insert->execute();
+          // Commit our transaction.
+          unset($transaction);
+
+          // Now we should try to find entities that match these items and invalidate their cache tags.
         }
-        $insert->execute();
-        // Commit our transaction.
-        unset($transaction);
-
-        // Now we should try to find entities that match these items and invalidate their cache tags.
-
       }
     }
-
+    catch (RequestException $e) {
+      if ($e->hasResponse()) {
+        $this->logger->error('Got error code: @code and message: @message', [
+          '@code' => $e->getCode(),
+          '@message' => (string) $e->getResponse()->getBody(),
+        ]);
+      }
+      else {
+        $this->logger->error('Got exception: @message', [
+          '@message' => $e->getMessage(),
+        ]);
+      }
+    }
   }
 
 }
